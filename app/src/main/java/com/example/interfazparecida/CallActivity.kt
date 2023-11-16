c72011e7b2f4c02812d0350b2ce89689b06ae279
Victorrudmo <victorrudmo@gmail.com>
2023-11-16 18:36:58 +0100

    first commit

---
+++ app/src/main/java/com/example/interfazparecida/CallActivity.kt
@@ -0,0 +1,27 @@
+package com.example.interfazparecida
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class CallActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_call)
+
+        val phoneNumber = intent.getStringExtra("phone_number")
+
+        val confirmCallButton = findViewById<Button>(R.id.callButton2)
+        confirmCallButton.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_CALL).apply {
+                data = Uri.parse("tel:$phoneNumber")
+            }
+            startActivity(callIntent)
+        }
+    }
+}
